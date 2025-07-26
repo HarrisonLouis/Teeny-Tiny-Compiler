a991328ca23aea22b990dbe69dc37de4a7d0ebfd
Harrison Louis <harrisonmlouis@gmail.com>
2025-07-26 11:59:54 -0500

    first commit

---
+++ teenytiny.py
@@ -0,0 +1,20 @@
+from lex import *
+from parse import *
+
+
+def main():
+    print("Teeny Tiny Compiler")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], "r") as inputFile:
+        source = inputFile.read()
+
+    lexer = Lexer(source)
+    parser = Parser(lexer)
+
+    parser.program()
+    print("Parsing completed.")
+
+
+main()
